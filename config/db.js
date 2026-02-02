0cf6297e7e645556f95bf67c779b0bd9178b8ef3
SRIRAM P <165500350+Sriramofficialsit@users.noreply.github.com>
2026-02-02 20:28:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const dbconnect = () => {
+const dbconnect = async() => {
   try {
-    mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database connected successfully");
   } catch (error) {
     return error.message;
